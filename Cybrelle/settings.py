4692db0e757c8db4453eaf0e294e6828b51f58e5
keinelain <laineytubbs@protonmail.com>
2023-01-28 16:34:00 -0600

    Fixed static again pt2

--- Cybrelle/settings.py
+++ Cybrelle/settings.py
@@ -173,7 +173,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL =  os.path.join(BASE_DIR, 'frontend/')
-STATIC_ROOT   =  os.path.join(BASE_DIR, 'frontend/')
+STATIC_ROOT   =  os.path.join(BASE_DIR, 'frontend/static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend')
 ]
