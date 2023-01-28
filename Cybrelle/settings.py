232cf4ededf7219f7bac8f407c8637bce6a1bf31
keinelain <laineytubbs@protonmail.com>
2023-01-28 16:02:52 -0600

    change allowed hosts

--- Cybrelle/settings.py
+++ Cybrelle/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','cybrelle.io', 'Cybrelle.io', 'Cybrelle.com', 'cybrelle.com']
+ALLOWED_HOSTS = ['127.0.0.1','206.189.180.236','cybrelle.io']
 
 
 # Application definition
