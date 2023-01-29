00c9a507d24b83b035b2e1ce3d4f0b8b10bd37dd
keinelain <laineytubbs@protonmail.com>
2023-01-29 15:45:22 -0600

    fixed dockerfile

--- Cybrelle/wsgi.py
+++ Cybrelle/wsgi.py
@@ -10,9 +10,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from fastapi import FastAPI
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cybrelle.settings')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Cybrelle.settings'
+
 
 application = get_wsgi_application()
 
