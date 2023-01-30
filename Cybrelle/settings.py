9623078fac27af49a763e318a814af5582e70e98
keinelain <laineytubbs@protonmail.com>
2023-01-29 19:20:00 -0600

    fixed allowed hosts

--- Cybrelle/settings.py
+++ Cybrelle/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','cybrelle.io']
+ALLOWED_HOSTS = ['0.0.0.0','cybrelle.io']
 
 
 # Application definition
