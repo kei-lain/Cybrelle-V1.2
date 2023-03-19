3b4c5b3ac69c5bfef0d166c6a94724d93dff25ba
keinelain <laineytubbs@protonmail.com>
2023-03-19 12:59:42 -0500

    added csrf trusted origins

--- Cybrelle/settings.py
+++ Cybrelle/settings.py
@@ -180,6 +180,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS=['https://cybrelle.io']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
